76b2f2dfa08f9fc9a9aaabaf4837028a78ab627c
Viswanath Nuggu <nugguviswanathcse@gmail.com>
2016-05-15 10:20:14 -0500

    overloaded add() in LLOperations class

--- DataStructures/src/com/linkedlist/LLOperations.java
+++ DataStructures/src/com/linkedlist/LLOperations.java
@@ -14,8 +14,29 @@ public class LLOperations {
 		head = new Node(null);
 	}
 
+	// below add method is used to insert node at the end of the LList
+	public void add(int data) {
+		Node temp = new Node(data, null);
+		Node current = head;
+		while (current.getNext() != null) {
+			current = current.getNext();// traversing to next node
+		}
+		/*
+		 * for the new node we are setting its next node as current nodes next
+		 * node
+		 */
+
+		temp.setNext(current.getNext());
+		// now we will add our new node (temp) to the linked list
+		current.setNext(temp);
+	}
+
+	/*
+	 * below add() is an overloaded method, used to insert new node at specified
+	 * position
+	 */
 	public void add(int data, int position) {
-		Node temp = new Node(data,null);
+		Node temp = new Node(data, null);
 		Node current = head;
 		for (int i = 0; i < position && current.getNext() != null; i++) {
 			current = current.getNext();// traversing to next node
@@ -24,7 +45,7 @@ public class LLOperations {
 		 * for the new node we are setting its next node as current nodes next
 		 * node
 		 */
-		
+
 		temp.setNext(current.getNext());
 		// now we will add our new node (temp) to the linked list
 		current.setNext(temp);
@@ -32,7 +53,7 @@ public class LLOperations {
 
 	public void printLL() {
 		Node ll = new Node(null);
-//		System.out.print(head.getData());
+		// System.out.print(head.getData());
 		ll = head.getNext();
 		while (ll.next != null) {
 			System.out.print(ll.getData());
--- DataStructures/src/com/linkedlist/LList.java
+++ DataStructures/src/com/linkedlist/LList.java
@@ -14,11 +14,12 @@ public class LList {
 	 */
 	public static void main(String[] args) {
 		LLOperations list = new LLOperations();
-		list.add(8,0);
+		list.add(8);//initially adding at head
 		list.add(9,2);
 		list.add(4,3);
 		list.add(13,0);
 		list.add(12,1);
+		list.add(99);//adding at tail
 		list.printLL();
 	}
 
